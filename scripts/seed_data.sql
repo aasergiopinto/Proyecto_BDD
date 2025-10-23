5d0bd0e51c203aaa7bafb641fe867521f164b39b
JoakinMacAuliffe <jfmacauliffe@gmail.com>
2025-10-23 16:54:40 -0300

    modelo

--- scripts/seed_data.sql
+++ scripts/seed_data.sql
@@ -1,14 +1,15 @@
 -- Productos
 INSERT INTO producto (id_producto, nombre_producto, tipo, fecha_lanzamiento, valor) VALUES
-(1, 'Smartphone X', 'Electrónica', '2023-03-01', 800000),
-(2, 'Zapatos deportivos ProRun', 'Calzado', '2022-11-15', 65000),
-(3, 'Laptop NovaBook', 'Computadores', '2023-08-20', 1200000);
+(1, 'Cuenta Más Lucas', 'Cuenta Vista', '2023-01-01', 0),
+(2, 'Plan Estudiantil', 'Cuenta Vista', '2022-01-01', 0),
+(3, 'Crédito Hipotecario', 'Crédito Hipotecario', '2023-08-20', 0),
+(4, 'Tarjeta Crédito Futura', 'Cuenta Corriente', '2000-01-01', 60000)
 
 -- Acciones comerciales
 INSERT INTO accion_comercial (id_accion, nombre_accion, objetivo, presupuesto, fecha_inicio, id_producto) VALUES
-(1, 'Campaña lanzamiento Smartphone X', 'Aumentar ventas iniciales', 20000000, '2023-03-10', 1),
-(2, 'Promoción regreso a clases', 'Incrementar ventas escolares', 15000000, '2023-02-01', 3),
-(3, 'Descuento verano ProRun', 'Reducir inventario de temporada', 10000000, '2023-12-01', 2);
+(1, 'Campaña lanzamiento Cuenta Más Lucas', 'Promocionar Cuenta Más Lucas', 20000000, '2023-03-10', 1),
+(2, 'Promoción regreso a clases', 'Promocionar Plan Estudiantil a estudiantes', 15000000, '2022-01-01', 2),
+(3, 'Descuento Italiano Veloz', 'Persuadir estudiantes a utilizar su cuenta vista en Italiano Veloz', 10000000, '2023-12-01', 2);
 
 -- Canales de difusión
 INSERT INTO canal_difusion (id_canal, nombre_canal, tipo) VALUES
@@ -21,15 +22,23 @@ INSERT INTO segmento (id_segmento, tamano_segmento, nombre_segmento, criterios) 
 (1, 5000, 'Jóvenes profesionales', 'Edad 25-35, nivel medio-alto de ingresos'),
 (2, 3000, 'Estudiantes universitarios', 'Edad 18-25, ingresos bajos'),
 (3, 2000, 'Deportistas', 'Interés en fitness y vida activa');
+(4, 1000, 'Profesionales', 'Interes en créditos hipotecarios y tarjetas de crédito')
 
 -- Clientes
 INSERT INTO cliente (rut, nombre_cliente, fecha_nacimiento, ingresos_mensuales, apellido_paterno, apellido_materno, id_segmento) VALUES
+('21.817.550-3', 'Joakin', '2005-04-16', 50000, 'Mac-Auliffe', 'López', 2),
+('21.827.932-5', 'Sergio', '2005-04-23', 0, 'Pinto', 'Tapia', 2),
+('21.662.158-1', 'Jordi', '2004-09-09', 48000, 'Farías', 'Araya', 2),
+('11.111.111-2', 'Carlos', '1975-01-01', 3000000, 'Sánchez', 'Sánchez', 4),
 ('11.111.111-1', 'Carlos', '1990-05-12', 1200000, 'Pérez', 'Gómez', 1),
 ('22.222.222-2', 'María', '2001-03-10', 350000, 'López', 'Soto', 2),
 ('33.333.333-3', 'Andrés', '1995-09-25', 800000, 'García', 'Muñoz', 3);
 
 -- Teléfonos
 INSERT INTO telefono_cliente (rut_cliente, telefono) VALUES
+('21.817.550-3', '+56942938546'),
+('21.827.932-5', '+56992231353'),
+('21.662.158-1', '+56991326190'),
 ('11.111.111-1', '+56911111111'),
 ('22.222.222-2', '+56922222222'),
 ('33.333.333-3', '+56933333333');
